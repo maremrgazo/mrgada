39cff4c4a2001bbd1c3ac5258ce646ef33b0fb05
maremrgazo <lazarevic.marko888@gmail.com>
2024-11-23 14:00:21 +0100

    .

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 using Opc.UaFx.Client;
 
 Mrgada.Initialize("192.168.64.121", Mrgada.MachineType.Client);
-
+//Mrgada.Initialize("0.0.0.0", Mrgada.MachineType.Server);
 
 Mrgada.MRP6 MRP6 = new("MRP6", Mrgada.AcquisitorType.S71500, "192.168.64.177", 61100, 200);
 Mrgada.Acquisitor APISKID = new("APISKID", Mrgada.AcquisitorType.S71500, "192.168.64.188", 61101, 200);
